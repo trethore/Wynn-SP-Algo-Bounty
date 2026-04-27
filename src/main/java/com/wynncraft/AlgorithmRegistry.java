1799d64fa8f488b788a780b0f7f7a606e3195207
Tytoo <titou.rethore@gmail.com>
2026-04-27 15:51:03 +0200

    feat: StarvingGoblinAlgorithm (#9)
    * feat: add StarvingGoblinAlgorithm and StarvingPlayer classes
    
    * feat: optimised StarvingGoblinAlgorithm and StarvingPlayer classes
    
    * chore: relink ours algos in AlgorithmRegistry

--- src/main/java/com/wynncraft/AlgorithmRegistry.java
+++ src/main/java/com/wynncraft/AlgorithmRegistry.java
@@ -3,12 +3,10 @@ package com.wynncraft;
 
 import com.wynncraft.algorithms.NegativeOrderAlgorithm;
 import com.wynncraft.algorithms.CapyTopoAlgorithm;
-import com.wynncraft.algorithms.melon.HungryGoblinAlgorithm;
+import com.wynncraft.algorithms.melon.*;
 import com.wynncraft.algorithms.PrunedMaskAlgorithm;
 import com.wynncraft.algorithms.PrunedMaskV2Algorithm;
 import com.wynncraft.algorithms.WynnFrumaAlgorithm;
-import com.wynncraft.algorithms.melon.EDGNAlgorithm;
-import com.wynncraft.algorithms.melon.HungryPlayer;
 import com.wynncraft.core.WynnPlayer;
 import com.wynncraft.core.interfaces.IAlgorithm;
 import com.wynncraft.core.interfaces.IPlayerBuilder;
@@ -27,11 +25,13 @@ public class AlgorithmRegistry {
         // New additions always goes on the bottom for reference
         register(new WynnFrumaAlgorithm(), WynnPlayer.Builder::new);
         register(new CapyTopoAlgorithm(), WynnPlayer.Builder::new);
-        register(new EDGNAlgorithm(), WynnPlayer.Builder::new);
-        register(new HungryGoblinAlgorithm(), HungryPlayer.Builder::new);
         register(new NegativeOrderAlgorithm(), WynnPlayer.Builder::new);
         register(new PrunedMaskAlgorithm(), WynnPlayer.Builder::new);
         register(new PrunedMaskV2Algorithm(), WynnPlayer.Builder::new);
+
+        register(new EDGNAlgorithm(), WynnPlayer.Builder::new);
+        register(new HungryGoblinAlgorithm(), HungryPlayer.Builder::new);
+        register(new StarvingGoblinAlgorithm(),StarvingPlayer.Builder::new);
     }
 
     /**
