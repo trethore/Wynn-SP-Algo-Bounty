ab22df8f36d0e260aa7ac4dd7bcd79431ae8cda9
trethore <titou.rethore@gmail.com>
2026-04-28 12:43:22 +0200

    feat: add CuriousAlgorythm and CuriousPlayer classes to AlgorithmRegistry

--- src/main/java/com/wynncraft/AlgorithmRegistry.java
+++ src/main/java/com/wynncraft/AlgorithmRegistry.java
@@ -33,6 +33,7 @@ public class AlgorithmRegistry {
         register(new LeSprintAlgorithm(), LeSprintPlayer.Builder::new);
         register(new LeMelonAlgorithm(), LeMelonPlayer.Builder::new);
         register(new HungryMelonEater(), LeFastPlayer.Builder::new);
+        register(new CuriousAlgorythm(), CuriousPlayer.Builder::new);
     }
 
     /**
